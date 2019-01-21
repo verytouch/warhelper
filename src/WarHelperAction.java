96faedbb3fd44a9addb9e19166bfe6313bc7b390
verytouch <verytouch@qq.com>
2019-01-21 17:22:46 +0800

    clean code

--- src/WarHelperAction.java
+++ src/WarHelperAction.java
@@ -1,19 +1,6 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.izhonghong.plugin.idea.warhelper.core.DefaultWarHelper;
-import com.izhonghong.plugin.idea.warhelper.core.WarHelper;
 import com.izhonghong.plugin.idea.warhelper.ui.WarHelperDialog;
-import com.thaiopensource.validate.StringOption;
-import org.eclipse.jdt.internal.compiler.lookup.InferenceContext;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Optional;
 
 public class WarHelperAction extends AnAction {
 
